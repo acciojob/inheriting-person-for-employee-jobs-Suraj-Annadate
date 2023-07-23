51d6c15cfc63277c44cdadb1e0565f40397b2dd4
Suraj Annadate <124056028+Suraj-Annadate@users.noreply.github.com>
2023-07-23 17:28:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,8 +8,6 @@ Person.prototype.greet = function(){
 	console.log('Hello, my name is ${this.name}, I am ${this.age} years old')
 }
 
-
-
 function Employee(name, age, jobTitle) {
 	Person.call(this,name,age)
 	this.jobTitile = jobTitle
@@ -21,6 +19,12 @@ Employee.prototype.constructor = Employee
 Employee.prototype.jobGreet = function(){
 	console.log('Hello, my name is ${this.name}, I am ${this.age}, and my job title is ${this.jobTile}')
 }
+const person = new Person('Suraj',25)
+person.greet()
+
+const employee = new Employee('Pankaj', 24, 'SDE')
+employee.greet()
+employee.jobGreet()
 
 // Do not change code below this line
 window.Person = Person;
